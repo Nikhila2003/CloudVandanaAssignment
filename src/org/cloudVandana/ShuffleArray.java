149350686dbdcbfdee3f915ef906a402f491501a
Nikhila <nikhila20003@gmail.com>
2023-10-17 19:46:15 +0530

    this is third commit

---
+++ src/org/cloudVandana/ShuffleArray.java
@@ -0,0 +1,36 @@
+package org.cloudVandana;
+
+import java.util.ArrayList;
+import java.util.Arrays;
+import java.util.Random;
+
+public class ShuffleArray {
+
+	public static void main(String[] args) {
+		
+		int[] arr = {1, 2, 3, 4, 5, 6, 7};
+
+        System.out.println(arr);
+
+        // Shuffle the array
+        shuffleArray(arr);
+
+        System.out.println("Shuffled array: " + Arrays.toString(arr));
+    }
+
+    private static void shuffleArray(int[] arr) {
+        Random rand = new Random();
+
+        for (int i = arr.length - 1; i > 0; i--) {
+            int index = rand.nextInt(i + 1);
+
+            // Swap the elements at positions i and index
+            int temp = arr[i];
+            arr[i] = arr[index];
+            arr[index] = temp;
+        }
+    }
+}
+
+
+
